ceb879e3ce1d3234a25eac09a9ee224108a8104e
wyld-fyre <gunnar.finkeldeh@gmail.com>
2022-05-03 10:33:05 +0100

    changed login script

--- cypress/integration/1-login/login.spec.js
+++ cypress/integration/1-login/login.spec.js
@@ -7,20 +7,13 @@
 
 describe('Dashboard Login Page', () => {
     it('Opens the Dashboard Login Page', () => {
-      cy.visit('https://insights.streetbees.com/streetbees-staging/demoboard-v2/login').contains('OK').click()
 
-      cy.get('[name="user"]')
-      .type('gunnar.finkeldeh@streetbees.com')
-      .should('value', 'gunnar.finkeldeh@streetbees.com')
-      
-      cy.get('[name="password"]')
-      .type('8pbgdm!Gtmd2UeY')
-      .should('value', '8pbgdm!Gtmd2UeY')
-      
-      cy.get('[type="submit"]').click()
+      const name = ('gunnar.finkeldeh@streetbees.com')
+      const password = ('8pbgdm!Gtmd2UeY')
 
-      cy.get('[class="heading-font line-height-1 h1 charcoal bold pr1"]').contains('Food Demand Spaces')
-
-      cy.url().should('include', 'demoboard-v2/spaces')
+      cy.visit('https://insights.streetbees.com/streetbees-staging/demoboard-v2/login')
+          cy.get('[name="user"]').type(name)   
+          cy.get('[name="password"]').type(password)    
+          cy.get('[type="submit"]').click()
+      })
     })
-})
