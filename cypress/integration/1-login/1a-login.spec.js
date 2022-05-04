13aee0fdbc5c3c386c2e536d2d486c28ad028dfe
wyld-fyre <gunnar.finkeldeh@gmail.com>
2022-05-04 11:54:27 +0100

    Added seelctin ga single Market test to Landing Page, added some comemnts to Landing, reworded and restructured test cases

--- cypress/integration/1-login/1a-login.spec.js
+++ cypress/integration/1-login/1a-login.spec.js
@@ -6,12 +6,13 @@
 // https://on.cypress.io/writing-first-test
 
 describe('Dashboard Login Page', () => {
-    it('Opens the Dashboard Login Page', () => {
+    it('Login (Valid) into Dashboard', () => {
 
       const name = ('gunnar.finkeldeh@streetbees.com')
       const password = ('8pbgdm!Gtmd2UeY')
 
       cy.visit('https://insights.streetbees.com/streetbees-staging/demoboard-v2/login')
+          cy.get('[class="CookiesPopup"').contains('OK').click()
           cy.get('[name="user"]').type(name)   
           cy.get('[name="password"]').type(password)    
           cy.get('[type="submit"]').click()
---
+++ cypress/integration/2-landing/2a-market.spec.js
@@ -0,0 +1,51 @@
+//Test Case: 
+// Testing the functionality of bein gable to select a Market and seeing the Landing Page change according to the Market Share
+// Expected Results:
+// 1. Marimekko changes
+// 2. Growth Matrix changes
+// 3. Spaces Comparison changes
+
+//
+// Selects a specific Market
+//
+
+describe('Selects a specific Market', () => {
+  it('Select Market, i.e UK', () => {
+    const dropdown = cy.get('.DropdownFilter').contains('.FilterItem > div:first-child', 'Market')
+
+    let select = dropdown.get('.Select')
+
+    select.first().click()
+
+    select.get('.Select-option').contains('United Kingdom').click()
+    cy.wait(1000)
+    cy.get('[style="height: 171.875px;"] > .SpacesMarimekko__Button').click()
+    cy.wait(5000)
+    cy.get('.BackButton').click()
+  });
+});
+
+
+//
+// Switches between different Markets
+//
+
+// describe('Switch between Markets', () => {
+//   it('Select Market from Dropdown', () => {
+//     const dropdown = cy.get('.DropdownFilter').contains('.FilterItem > div:first-child', 'Market')
+
+//     let select = dropdown.get('.Select')
+
+//     select.first().click()
+
+//     select.get('.Select-option').each((element, index) => {
+//       select.get('.Select-option').eq(index)
+//       cy.wait(1000)
+//       cy.wrap(element).click()
+//       cy.wait(1000)
+//       cy.get('[style="height: 171.875px;"] > .SpacesMarimekko__Button').click()
+//       cy.wait(5000)
+//       cy.get('.BackButton').click()
+//     })
+//   })
+// })
