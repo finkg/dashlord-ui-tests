91eaff6a5aa8ce05a0b1a64c52d6568fed1c0865
wyld-fyre <gunnar.finkeldeh@gmail.com>
2022-05-16 09:06:24 +0100

    removed login details from login spec, added env variables to cypress.json and baseUrl

--- cypress/integration/2-landing/2b-category.spec.js
+++ cypress/integration/2-landing/2b-category.spec.js
@@ -1,12 +1,12 @@
 describe('Select a Category', () => {
-    it('Select Category, i.e. Cereals', () => {
+    it('Select Category, i.e. Breads', () => {
         const dropdown = cy.get('.DropdownFilter').contains('.FilterItem > div:first-child', 'Category')
 
         let select = dropdown.get('.Select')
         select.last().click()
         cy.wait(2000)
 
-        select.get('.Select-option').contains('Cereals').click()
+        select.get('.Select-option').contains('Breads').click()
         // cy.wait(5000)
     });
 });
